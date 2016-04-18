c6aecb7d3cff480c0e4f5854c46d4d96a123ef81
TotalBollocks <jqrainwater@yahoo.com>
2016-04-18 18:56:43 -0500

    Ok

--- old.sh
+++ old.sh
@@ -1,6 +1,17 @@
+if [ -z $1 ]
+then
+
 for f in oldExamples/*; do
 	./a.out $f > output.txt
 	file=${f%.txt}
 	echo "Testing $file"
 	diff -w output.txt oldOutput/${file##*/}.txt.out
 done
+
+else
+
+echo Testing $1
+./a.out oldExamples/$1.txt > output.txt
+diff -w output.txt oldOutput/$1.txt.out
+
+fi
